15cd43951f9dc52e704d7eca243a373bcbd8f1ec
marbal88 <109217383+marbal88@users.noreply.github.com>
2022-08-15 20:50:04 +0200

    End of section 17

---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+import { useStore } from '../../app/stores/store';
+import ProfileContent from './ProfileContent';
+import ProfileHeader from './ProfileHeader';
+
+export default function ProfilePage() {
+    const {username} = useParams<{username: string}>();
+    const {profileStore} = useStore();
+    const {loadingProfile, loadProfile, profile} = profileStore;
+
+    useEffect(() => {
+        loadProfile(username);
+    }, [loadProfile, username])
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                {profile && 
+                <>
+                    <ProfileHeader profile={profile} />
+                    <ProfileContent profile={profile}/>
+                </>}
+            </Grid.Column>
+        </Grid>
+    )
+}
