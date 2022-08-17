a2f6b4775742ea245c0deeb66b6effd299156710
marbal88 <109217383+marbal88@users.noreply.github.com>
2022-08-17 11:55:24 +0200

    End of section 20

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -8,11 +8,14 @@ import ProfileHeader from './ProfileHeader';
 export default function ProfilePage() {
     const {username} = useParams<{username: string}>();
     const {profileStore} = useStore();
-    const {loadingProfile, loadProfile, profile} = profileStore;
+    const {loadingProfile, loadProfile, profile, setActiveTab} = profileStore;
 
     useEffect(() => {
         loadProfile(username);
-    }, [loadProfile, username])
+        return () => {
+            setActiveTab(0);
+        }
+    }, [loadProfile, username, setActiveTab])
     return (
         <Grid>
             <Grid.Column width={16}>
