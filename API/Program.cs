a05d62a985c0120f75e94b23bf5d2697f2af9d21
marbal88 <109217383+marbal88@users.noreply.github.com>
2022-08-21 23:25:42 +0200

    Security headears add

--- API/Program.cs
+++ API/Program.cs
@@ -27,15 +27,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCsp(opt => opt
-        .BlockAllMixedContent()
-        .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
-        .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
-        .FormActions(s => s.Self())
-        .FrameAncestors(s => s.Self())
-        .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
-        .ScriptSources(s => s.Self())
-);
+
 
 if (app.Environment.IsDevelopment())
     {
