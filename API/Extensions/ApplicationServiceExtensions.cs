4c389bb9a7ebeee52f281583a360edab4357b52d
marbal88 <109217383+marbal88@users.noreply.github.com>
2022-08-22 22:45:18 +0200

    refresh token complete

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -71,6 +71,7 @@ namespace API.Extensions
                                     .AllowAnyMethod()
                                     .AllowAnyHeader()
                                     .AllowCredentials()
+                                    .WithExposedHeaders("WWW-Authenticate", "Pagination")
                                     .WithOrigins("http://localhost:3000");
                             });
                         });
